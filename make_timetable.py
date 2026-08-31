2a9ecc150d2b4e3a06a4677b770ad3b6af834fa9
Alexey Osipov <osipovav28@googlemail.com>
2026-08-31 18:10:02 +0300

    Create make_timetable.py

---
+++ make_timetable.py
@@ -0,0 +1,25 @@
+def make_timetable(meetings):
+    if len(meetings) == 0:
+        return 0
+    if len(meetings) == 1:
+        return 1
+    meetings.sort()
+    start = 0
+    end = 0
+    max_number_of_rooms = 1
+    number_of_rooms = 0
+    while (start < len(meetings)) and (end < len(meetings)):
+        if end == start:
+            end += 1
+            number_of_rooms += 1
+            continue
+        if meetings[start][0] <= meetings[start][1] < meetings[end][1]:
+            number_of_rooms += 1
+            end += 1
+        else:
+            max_number_of_rooms = max(max_number_of_rooms, number_of_rooms)
+            start += 1
+            end = start
+            number_of_rooms = 0
+    max_number_of_rooms = max(max_number_of_rooms, number_of_rooms)
+    return max_number_of_rooms
